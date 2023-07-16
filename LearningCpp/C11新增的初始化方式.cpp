5212c0198e5ba740af72179be09e1197cf2e8893
LiuXuan2096 <liuxuan2096@icloud.com>
2023-07-16 17:22:24 +0800

    逗号运算符

--- LearningCpp/C11新增的初始化方式.cpp
+++ LearningCpp/C11新增的初始化方式.cpp
@@ -2,17 +2,17 @@
 
 using namespace std;
 
-int main()
-{
-	int a = 10;
-	int b = (10);
-	int c(10);
-	int e = { 10 };
-	int f{ 10 };
-
-	cout << "a= " << a << endl;
-	cout << "b= " << a << endl;
-	cout << "c= " << a << endl;
-	cout << "d= " << a << endl;
-	cout << "e= " << a << endl;
-}
+//int main()
+//{
+//	int a = 10;
+//	int b = (10);
+//	int c(10);
+//	int e = { 10 };
+//	int f{ 10 };
+//
+//	cout << "a= " << a << endl;
+//	cout << "b= " << a << endl;
+//	cout << "c= " << a << endl;
+//	cout << "d= " << a << endl;
+//	cout << "e= " << a << endl;
+//}
---
+++ LearningCpp/逗号运算符.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+逗号运算符：把一行语句中的多个表达式连接起来，程序将从左到右执行表达式。
+*/
+
+int main()
+{
+	int a = 10, b = 20;
+	cout << "a= " << a << endl;
+	cout << "b= " << b << endl;
+}
