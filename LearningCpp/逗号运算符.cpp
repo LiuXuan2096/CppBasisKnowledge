3efe2515efa88c2a76eeb31baf4e877b573049ca
LiuXuan2096 <liuxuan2096@icloud.com>
2023-07-17 22:37:43 +0800

    2023-07-17

--- LearningCpp/逗号运算符.cpp
+++ LearningCpp/逗号运算符.cpp
@@ -6,9 +6,9 @@ using namespace std;
 逗号运算符：把一行语句中的多个表达式连接起来，程序将从左到右执行表达式。
 */
 
-int main()
-{
-	int a = 10, b = 20;
-	cout << "a= " << a << endl;
-	cout << "b= " << b << endl;
-}
+//int main()
+//{
+//	int a = 10, b = 20;
+//	cout << "a= " << a << endl;
+//	cout << "b= " << b << endl;
+//}
